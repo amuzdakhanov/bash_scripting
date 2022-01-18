016de0b55289d8d1a89a7cac119de69818e35034
Arystan Muzdakhanov <amuzdakhanov@gmail.com>
2022-01-18 23:35:58 +0600

    added some scripts

---
+++ 14_professional_menu/1.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+url="https://www.dundeecity.gov.uk/sites/default/files/publications/civic_renewal_forms.zip"
+#curl ${url} -O  // download original name file
+curl ${url} -o  NEWFILE
+  
